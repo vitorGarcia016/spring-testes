0687a32ea8a5dc503da8e2755ba8b7f165baded6
vitorGarcia016 <vitorgarciaoliveira2005@gmail.com>
2026-01-13 17:52:18 -0300

    Create CarroRepository.java

---
+++ src/main/java/com/vitor/spring_testes/repository/CarroRepository.java
@@ -0,0 +1,7 @@
+package com.vitor.spring_testes.repository;
+
+import com.vitor.spring_testes.entity.CarroEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarroRepository extends JpaRepository<CarroEntity,Integer> {
+}
