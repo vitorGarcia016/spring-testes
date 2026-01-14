efbcb4789566dff359e1c20c88af801937622f35
vitorGarcia016 <vitorgarciaoliveira2005@gmail.com>
2026-01-14 16:38:41 -0300

    Update CarroEntity.java

--- src/main/java/com/vitor/spring_testes/entity/CarroEntity.java
+++ src/main/java/com/vitor/spring_testes/entity/CarroEntity.java
@@ -1,11 +1,9 @@
 package com.vitor.spring_testes.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "carro")
 public class CarroEntity {
 
     @Id
