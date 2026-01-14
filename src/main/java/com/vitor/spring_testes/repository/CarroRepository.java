f2793a7b611b47f1bc787dd28dda5e85dad67942
vitorGarcia016 <vitorgarciaoliveira2005@gmail.com>
2026-01-14 16:38:37 -0300

    Update CarroRepository.java

--- src/main/java/com/vitor/spring_testes/repository/CarroRepository.java
+++ src/main/java/com/vitor/spring_testes/repository/CarroRepository.java
@@ -4,4 +4,6 @@ import com.vitor.spring_testes.entity.CarroEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CarroRepository extends JpaRepository<CarroEntity,Integer> {
+
+    CarroEntity findByNome(String nome);
 }
